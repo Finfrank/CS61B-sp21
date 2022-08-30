04284b913047c17a22c5367a4e7036476437af3c
Finfrank <lfh142857@gmail.com>
2022-08-30 18:23:09 +0800

    proj1 finished

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,47 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class GuitarHero {
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        /* create two guitar strings, for concert A and C */
+        GuitarString[] guitarStrings = new GuitarString[keyboard.length()];
+
+        for (int i = 0; i < keyboard.length(); i++) {
+            double frequency = 440 * Math.pow(2, (i - 24) / 12.0);
+            guitarStrings[i] = new GuitarString(frequency);
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index > 0) {
+                    guitarStrings[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (int i = 0; i < keyboard.length(); i++) {
+                sample += guitarStrings[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < keyboard.length(); i++) {
+                guitarStrings[i].tic();
+            }
+        }
+    }
+}
+
